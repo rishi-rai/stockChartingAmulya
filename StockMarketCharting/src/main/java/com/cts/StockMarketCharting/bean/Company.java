649cbca142425718b4e8645ac12c2abeaa8a1547
rishi-rai <34191328+rishi-rai@users.noreply.github.com>
2019-08-07 15:47:03 +0530

    Add files via upload

---
+++ StockMarketCharting/src/main/java/com/cts/StockMarketCharting/bean/Company.java
@@ -0,0 +1,11 @@
+package com.cts.StockMarketCharting.bean;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name="Company")
+@Table(name="Company")
+public class Company {
+
+}
---
+++ StockMarketCharting/src/main/java/com/cts/StockMarketCharting/bean/User.java
@@ -0,0 +1,5 @@
+package com.cts.StockMarketCharting.bean;
+
+public class User {
+
+}
